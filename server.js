b550689c48d5860f3643c31f600ed0a08a888bea
João Marques <jonny1995@live.com.pt>
2018-08-23 20:08:34 +0100

    #2 port changed

--- server.js
+++ server.js
@@ -36,8 +36,8 @@ app.put('/image', (req, res) => {image.handleImage(req, res, db)});
 
 app.post('/imageurl', (req, res) => {image.handleAPICall(req, res)});
 
-app.listen(3000, ()=> {
-    console.log('app is running');
+app.listen(process.env.PORT || 3000, ()=> {
+    console.log('app is running ${process.env.PORT}');
 });
 
 
